1d88d1c8a1698b08d96fb9539e8a7edb57f5ae43
zask17 <zaskiarania5@gmail.com>
2026-06-02 11:56:51 +0700

    foldering screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+import 'screens/auth/splash_screen.dart';
 import 'theme/app_theme.dart';
 import 'data/theme_settings.dart';
 
